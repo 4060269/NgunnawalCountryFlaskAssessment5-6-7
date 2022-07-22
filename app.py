97f81a91aa70b147a27f4c0ca92f6b2b44f4004b
4060269 <4060269@schoolsnet.act.edu.au>
2022-07-22 11:32:05 +1000

    new config.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)  # loads the configuration for the database
+db = SQLAlchemy(app)            # creates the db object using the configuration
 
 
 @app.route('/')
