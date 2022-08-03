eff3dca3faaa2c98637bb8665a93e9961e7a7f11
4060269 <4060269@schoolsnet.act.edu.au>
2022-08-03 12:42:15 +1000

    update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import Flask, render_template
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from models import Contact
+from forms import ContactForm
 
 app = Flask(__name__)
 app.config.from_object(Config)  # loads the configuration for the database
@@ -12,6 +14,10 @@ db = SQLAlchemy(app)            # creates the db object using the configuration
 def aboutpage():  # put application's code here
     return render_template("index.html", title="Ngunnawal Country | About")
 
+@app.route("/contact.html", methods=["POST", "GET"])
+def contact():
+    form = ContactForm()
+    return render_template("contact.html", title ="Contact Us", form=form)
 
 if __name__ == '__main__':
     app.run()
