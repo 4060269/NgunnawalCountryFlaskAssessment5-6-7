6acdead2ad7890b55fb36906fc10a79074d00df1
4060269 <4060269@schoolsnet.act.edu.au>
2022-08-03 12:35:32 +1000

    update models.py

--- models.py
+++ models.py
@@ -2,3 +2,14 @@ from app import db
 from datetime import datetime
 
 class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text)
+    email = db.Column(db.Text)
+    message = db.Column(db.Text)
+    dateSubmitted = db.Column(db.DateTime)
+
+def __init__(self, name, email, message):
+    self.name = name
+    self.email = email
+    self.message = message
+    self.dateSubmitted = datetime.today()
