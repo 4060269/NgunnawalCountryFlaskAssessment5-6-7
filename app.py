99887d2ff91fe3f5da5a0beb966678148ce3d526
4060269 <4060269@schoolsnet.act.edu.au>
2022-09-14 12:39:11 +1000

    add alertbox

--- app.py
+++ app.py
@@ -131,6 +131,9 @@ def profile():
 @app.route('/contact_messages')
 @login_required
 def view_contact_messages():
-    contact_messages = Contact.query.all()
-    return render_template("contactMessages.html", title="Contact Messages", user=current_user, messages=contact_messages)
+    if current_user.is_admin():
+        contact_messages = Contact.query.all()
+        return render_template("contactMessages.html", title="Contact Messages", user=current_user, messages=contact_messages)
+    else:
+        return redirect(url_for("homepage"))
 
