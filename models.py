3bdc1f5b989cf19281d04c08776ab9748d3d25c2
4060269 <4060269@schoolsnet.act.edu.au>
2022-10-08 00:40:18 +1100

    comment updates to models.py

--- models.py
+++ models.py
@@ -1,29 +1,41 @@
-from app import db, login  # get access to the db by creating instance
-from datetime import datetime  # create an instance of real-time
-from flask_login import UserMixin  # imports userMixin, provides default values for user objects
-from werkzeug.security import generate_password_hash, \
-    check_password_hash  # import from werkzeug security to give hashing functionality
+# Models.py is used to define the db in the web app
+# Allows us to, define meta-data, tables and fields in db
+
+from app import db, login
+# Get access to the db for tables and forms
+from datetime import datetime
+# Need to log messages with the current date and time
+from flask_login import UserMixin
+# Creates easier development with all the default values
+from werkzeug.security import generate_password_hash, check_password_hash
+# Import from werkzeug security to provide hashing functionality
 
 
-class Contact(db.Model):  # class is to store db in memory
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # equates the database values to simple variables
-    name = db.Column(db.Text)  # these variables are stored under the contact class
-    email = db.Column(db.Text)  #
-    message = db.Column(db.Text)  #
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text)
+    email = db.Column(db.Text)
+    message = db.Column(db.Text)
     dateSubmitted = db.Column(db.DateTime)
+    # Class is to store db in memory
+    # Need to have a model of db to take and send tables and fields
+    # Equates the database values to simple local variables
+    # To organize and re-send to database correctly
 
-    def __init__(self, name, email, message):  # auto runs when new contact created,
-        self.name = name  # these are initalizaing the objects attriututes
-        self.email = email  # this is always needed for any object created from a class
+    def __init__(self, name, email, message):
+        self.name = name
+        self.email = email
         self.message = message
-        self.dateSubmitted = datetime.today()  # this line sets the datetime to be taken from the local computers date & time
+        self.dateSubmitted = datetime.today()
+        # Needed to define datetime to be taken from the local computers date & time
 
 
-class todo(db.Model):  # same thing as contact class
-    id = db.Column(db.Integer, primary_key=True,
-                   autoincrement=True)  # less entries and still auto creates a new id for every entry
-    text = db.Column(db.Text)  # I don't 'create' the init but it is still there
+class todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.Text)
     done = db.Column(db.Boolean)
+    # Needed to create, track and determine whether an item is completed or not
+    # Accessing columnar database
 
 
 class User(UserMixin, db.Model):
@@ -32,6 +44,7 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
     user_level = db.Column(db.Integer)
+    # UserMixin is required to get properties of a user to be displayed
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -61,9 +74,11 @@ class Photos(db.Model):
     filename = db.Column(db.String(255))
     userid = db.Column(db.Integer)
     dateSubmitted = db.Column(db.DateTime)
+    #
 
     def __init__(self, title, filename, userid):
         self.title = title
         self.filename = filename
         self.userid = userid
         self.dateSubmitted = datetime.today()
+        # Same as contact init, need to set date and time to client computer
